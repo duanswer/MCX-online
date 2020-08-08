18839bc1107a6e0513aed4efbadc8273dedc436a
Eduardo Answer <eduardonzinga111@hotmail.com>
2020-08-08 05:31:08 +0100

    First commit: Initial configurations

---
+++ android/app/src/main/kotlin/com/example/atm_online/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atm_online
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:atm_online/ui/IntroPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: IntroPage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
