56bed5b5f408484593ff0e4999371f8fdec126de
Eduardo Answer <eduardonzinga111@hotmail.com>
2020-08-08 06:31:01 +0100

    IntroPage concluded

--- lib/ui/IntroPage.dart
+++ lib/ui/IntroPage.dart
@@ -1,3 +1,4 @@
+import 'package:atm_online/ui/HomePage.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatefulWidget {
@@ -8,6 +9,97 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.orange, Colors.orangeAccent],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "MCX Online",
+                      style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            _buildFooter(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFooter(BuildContext context) {
+    return Positioned(
+      bottom: 50,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Aplicativo Multicaixa Online",
+              style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "é um aplicativo que lhe permite ver todos os ATMs\do território angolano",
+              style:
+                  TextStyle(fontSize: 18.0, color: Colors.white, height: 1.5),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 25),
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => HomePage(),
+                  ),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(1, 1),
+                          spreadRadius: 1,
+                          blurRadius: 3)
+                    ]),
+                width: MediaQuery.of(context).size.width * .85,
+                height: 60,
+                child: Center(
+                  child: Text("COMEÇAR",
+                      style: TextStyle(
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0)),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
