ff9f2d1914adee65cd72683d39518dc8a9737063
Eduardo Answer <eduardonzinga111@hotmail.com>
2020-08-09 16:28:14 +0100

    Continuing confguration

--- lib/ui/tabs/widgets/shoose_atm.dart
+++ lib/ui/tabs/widgets/shoose_atm.dart
@@ -1,3 +1,4 @@
+import 'package:atm_online/ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class ChooseAtm extends StatefulWidget {
@@ -7,15 +8,48 @@ class ChooseAtm extends StatefulWidget {
 
 class _ChooseAtmState extends State<ChooseAtm> {
   List _listATM = ["Kimpa Vita", "Maculusso", "Viana", "Belas"];
+  List _stateAtm = [
+    "Com notas",
+    "Sem notas",
+    "Com notas",
+    "Com notas",
+    "Sem notas"
+  ];
 
   _openMap() {}
   _removeATM() {}
+  _addATM() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => HomeSreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Importante"),
+                  content: Text(
+                      "Para escolher seu ATM favorito, pressione um local no mapa."),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("Ok"),
+                      onPressed: () {
+                        _addATM();
+                      },
+                    )
+                  ],
+                );
+              });
+        },
         child: Icon(
           Icons.add,
           color: Colors.white,
@@ -29,6 +63,7 @@ class _ChooseAtmState extends State<ChooseAtm> {
             itemCount: _listATM.length,
             itemBuilder: (context, index) {
               String title = _listATM[index];
+              String subtitle = _stateAtm[index];
               return GestureDetector(
                 onTap: () {
                   _openMap();
@@ -36,6 +71,7 @@ class _ChooseAtmState extends State<ChooseAtm> {
                 child: Card(
                   child: ListTile(
                     title: Text(title),
+                    subtitle: Text(subtitle),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
